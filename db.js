de5632cec5281ee41e1aaec920f63b9aba898f50
Eleazar <21-04052@g.batstate-u.edu.ph>
2025-01-02 11:51:24 +0800

    updated db

--- db.js
+++ db.js
@@ -1,4 +1,17 @@
 require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL, // Use environment variable for security
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch((err) => console.error('Database connection error', err.stack));
+
 
 const { Pool } = require('pg');
 
