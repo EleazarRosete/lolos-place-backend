0736102e11438553b16daf2f2d3d09b4f29016f7
Eleazar <21-04052@g.batstate-u.edu.ph>
2025-04-26 14:27:45 +0800

    Completed

--- node/db.js
+++ node/db.js
@@ -1,23 +1,39 @@
+// require('dotenv').config();
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL, // Use environment variable for security
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// client.connect()
+//   .then(() => console.log('Connected to the database'))
+//   .catch((err) => console.error('Database connection error', err.stack));
+
+
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL || '', // Use DATABASE_URL for production
+//     ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // SSL only for production
+//     user: process.env.DB_USER || 'postgres', // Fallback for local development
+//     host: process.env.DB_HOST || 'localhost',
+//     database: process.env.DB_DATABASE || 'lolos-place',
+//     password: process.env.DB_PASSWORD || 'password',
+//     port: process.env.DB_PORT || 5432, // Default PostgreSQL port
+// });
+
+// module.exports = pool;
+
 require('dotenv').config();
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL, // Use environment variable for security
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-client.connect()
-  .then(() => console.log('Connected to the database'))
-  .catch((err) => console.error('Database connection error', err.stack));
 
 
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || '', // Use DATABASE_URL for production
-    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // SSL only for production
+
     user: process.env.DB_USER || 'postgres', // Fallback for local development
     host: process.env.DB_HOST || 'localhost',
     database: process.env.DB_DATABASE || 'lolos-place',
@@ -26,3 +42,4 @@ const pool = new Pool({
 });
 
 module.exports = pool;
+
